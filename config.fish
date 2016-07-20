5794b2d1bc88f598db029260ba73c99334da9f93
iampeterbanjo <peter.banjo@bottomline.com>
2016-07-20 14:44:58 +0100

    add fish_config_path

--- config.fish
+++ config.fish
@@ -2,7 +2,9 @@ set -U NODE_PATH (which node)
 set -U NPM_PATH (which npm)
 set -U fish_user_paths
 
-source ~/.config/fish/init.fish
+set -U fish_config_path ~/.config/fish
+
+source $fish_config_path/init.fish
 
 set fisher_home ~/.local/share/fisherman
 set fisher_config ~/.config/fisherman
