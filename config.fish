8d7e633c4468dd1a0d0326bb149968d79ed5f05e
iampeterbanjo <peter.banjo@bottomline.com>
2017-01-05 08:44:50 +0000

    create aliases folder and use that

--- config.fish
+++ config.fish
@@ -6,11 +6,11 @@ switch (uname)
 case Linux
     echo 'Setting fish_config_path for Tux!'
     set -U fish_config_path ~/.config/fish
-    set -U fish_config_os_file linux.fish
+    set -U fish_config_os_aliases linux.fish
 case Darwin
     echo 'Setting fish_config_path for Hexley!'
     set -U fish_config_path ~/.config/fish/conf.d
-    set -U fish_config_os_file osx.fish
+    set -U fish_config_os_aliases osx.fish
 case FreeBSD NetBSD DragonFly
     echo 'Setting fish_config_path for Beastie!'
 case '*'
@@ -20,6 +20,6 @@ end
 set -U NODE_PATH (which node)
 set -U NPM_PATH (which npm)
 
-source $fish_config_path/usr/aliases.fish
-source $fish_config_path/usr/$fish_config_os_file
+source $fish_config_path/aliases/aliases.fish
+source $fish_config_path/aliases/$fish_config_os_aliases
 
