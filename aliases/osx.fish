dea3d17cfe5893325ea974b2b70b35c3d01fde9b
iampeterbanjo <peter.banjo@bottomline.com>
2017-01-06 17:44:53 +0000

    fix sublime text alias

--- aliases/osx.fish
+++ aliases/osx.fish
@@ -1,5 +1,5 @@
 echo 'Setting osx aliases'
 
 alias editor          'vim '
-alias sublime         'open -a Sublime\ Text'
+alias sublime         '/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'
 
