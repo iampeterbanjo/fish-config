e92bd0fd276ac55599d2ce574e0892f99b962c7c
iampeterbanjo <peter.banjo@bottomline.com>
2017-01-09 08:54:45 +0000

    add alias for Visual studio code

--- aliases/osx.fish
+++ aliases/osx.fish
@@ -2,4 +2,4 @@ echo 'Setting osx aliases'
 
 alias editor          'vim '
 alias sublime         '/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'
-
+alias code            '/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code'
