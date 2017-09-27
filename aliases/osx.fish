e9067b59a88ab8fb08b487cfa2a1c1d09bd43d26
iampeterbanjo <peter.banjo@bottomline.com>
2017-09-27 16:47:33 +0100

    add chrome headless aliases

--- aliases/osx.fish
+++ aliases/osx.fish
@@ -3,3 +3,7 @@ echo 'Setting osx aliases'
 alias editor          'vim '
 alias sublime         '/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'
 alias code            '/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code'
+
+alias chrome           '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
+alias chrome-canary    '/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary'
+alias chromium         '/Applications/Chromium.app/Contents/MacOS/Chromium'
