c423d9c60a3499b231f851647ddd64801f01588d
Peter Banjo <peter.banjo@teccura.software>
2019-03-25 09:15:24 +0000

    change all aliases to abbr

--- aliases/aliases.fish
+++ aliases/aliases.fish
@@ -4,204 +4,204 @@ echo 'Setting aliases'
 # EDITORS
 #
 
-alias code '/usr/share/code/code'
+abbr -a code '/usr/share/code/code'
 
 #
 # SYSTEM
 #
 
-alias sudo 'sudo '
-alias ai 'sudo apt-get install'
-alias aar 'sudo apt-add-repository'
-alias au 'sudo apt-get update'
-alias arr 'sudo apt-remove-repository'
+abbr -a sudo 'sudo '
+abbr -a ai 'sudo apt-get install'
+abbr -a aar 'sudo apt-add-repository'
+abbr -a au 'sudo apt-get update'
+abbr -a arr 'sudo apt-remove-repository'
 
-alias omfi 'omf install'
-alias omfr 'omf remove'
+abbr -a omfi 'omf install'
+abbr -a omfr 'omf remove'
 
-alias shutdown 'sudo shutdown'
-alias reboot 'sudo reboot'
+abbr -a shutdown 'sudo shutdown'
+abbr -a reboot 'sudo reboot'
 
-alias asdfm   'setxkbmap gb -variant ,dvorak'
-alias asdfm_us  'setxkbmap us -variant ,dvorak'
-alias aoeum   'setxkbmap gb'
-alias aoeum_us  'setxkbmap us'
+abbr -a asdfm   'setxkbmap gb -variant ,dvorak'
+abbr -a asdfm_us  'setxkbmap us -variant ,dvorak'
+abbr -a aoeum   'setxkbmap gb'
+abbr -a aoeum_us  'setxkbmap us'
 
 #
 # SSH
 #
 
 
-alias sshnewkey 'ssh-keygen -t rsa -b 4096 -C' #email
-alias sshaddkey 'ssh-agent -s; ssh-add'
+abbr -a sshnewkey 'ssh-keygen -t rsa -b 4096 -C' #email
+abbr -a sshaddkey 'ssh-agent -s; ssh-add'
 
 #
 # MAINTENANCE
 #
 
-alias sudo 'sudo '
-alias aliases 'cat $fish_config_path/aliases.fish'
-alias editfish '$fish_config_path/config.fish'
-alias reloadfish 'source $fish_config_path/config.fish'
-alias edithosts 'sudo $1 /etc/hosts'
-alias sudo 'sudo '
-alias l 'ls -la'
-alias find_alias 'cat $fish_config_path/aliases/aliases.fish | fzf | string trim | read -l result; and echo $result'
+abbr -a sudo 'sudo '
+abbr -a aliases 'cat $fish_config_path/aliases.fish'
+abbr -a editfish '$fish_config_path/config.fish'
+abbr -a reloadfish 'source $fish_config_path/config.fish'
+abbr -a edithosts 'sudo $1 /etc/hosts'
+abbr -a sudo 'sudo '
+abbr -a l 'ls -la'
+abbr -a find_abbr -a 'cat $fish_config_path/aliases/aliases.fish | fzf | string trim | read -l result; and echo $result'
 
 #
 # DEVELOPMENT
 #
 
-alias bsync 'browser-sync start --server . --xip --files="**/*.css, **/*.js, **/*.html" --directory'
-alias testem-proxy 'browser-sync start --proxy localhost:7357 --xip --no-open --port 4000'
-alias rm-node-modules 'rm -rf ./node_modules'
+abbr -a bsync 'browser-sync start --server . --xip --files="**/*.css, **/*.js, **/*.html" --directory'
+abbr -a testem-proxy 'browser-sync start --proxy localhost:7357 --xip --no-open --port 4000'
+abbr -a rm-node-modules 'rm -rf ./node_modules'
 
-alias package-version 'cat package.json | jq ".version"'
-alias package-name 'cat package.json | jq ".name"'
-alias package-info 'cat package.json | jq ". | { name: .name, version: .version, description: .description, scripts: .scripts }"'
+abbr -a package-version 'cat package.json | jq ".version"'
+abbr -a package-name 'cat package.json | jq ".name"'
+abbr -a package-info 'cat package.json | jq ". | { name: .name, version: .version, description: .description, scripts: .scripts }"'
 
 #
 # GIT
 #
 
-alias g-email 'git config --global user.email'
-alias g-name 'git config --global user.name'
+abbr -a g-email 'git config --global user.email'
+abbr -a g-name 'git config --global user.name'
 
-alias ga 'git add -A'
-alias gac 'git add -A; git commit -am'
-alias guau 'git update-index --assume-unchanged'
-alias gunu 'git update-index --no-assume-unchanged'
+abbr -a ga 'git add -A'
+abbr -a gac 'git add -A; git commit -am'
+abbr -a guau 'git update-index --assume-unchanged'
+abbr -a gunu 'git update-index --no-assume-unchanged'
 
-alias gbr 'git branch'
-alias gbrso 'git branch --set-upstream-to=origin/(git rev-parse --abbrev-ref HEAD) bugfix/CIR-16693-file-import-setting'
+abbr -a gbr 'git branch'
+abbr -a gbrso 'git branch --set-upstream-to=origin/(git rev-parse --abbrev-ref HEAD) bugfix/CIR-16693-file-import-setting'
 # checkout branches using fzf search
-alias gbrco 'git branch | fzf | string trim | read -l result; and gco $result'
+abbr -a gbrco 'git branch | fzf | string trim | read -l result; and gco $result'
 
-alias gm 'git commit --amend -m'
+abbr -a gm 'git commit --amend -m'
 
-alias gc 'git commit -am'
-alias gcl 'git clone'
-alias gco 'git checkout'
-alias gcob 'git checkout -br'
+abbr -a gc 'git commit -am'
+abbr -a gcl 'git clone'
+abbr -a gco 'git checkout'
+abbr -a gcob 'git checkout -br'
 # git restore file from last checkout
-alias gcof 'git checkout -- ' #file
+abbr -a gcof 'git checkout -- ' #file
 
 # reset and remove folders
-alias gfx 'git clean -ffdx'
+abbr -a gfx 'git clean -ffdx'
 
-alias gd 'git diff'
-alias gro 'git remote get-url origin'
+abbr -a gd 'git diff'
+abbr -a gro 'git remote get-url origin'
 
 # git submodules
-alias gsub-add 'git submodule add'
-alias gsub-diff 'git diff; git submodule foreach "git diff"'
-alias gsub-push 'git push --recurse-submodules=on-demand'
-alias gsub-up 'git submodule update --remote'
+abbr -a gsub-add 'git submodule add'
+abbr -a gsub-diff 'git diff; git submodule foreach "git diff"'
+abbr -a gsub-push 'git push --recurse-submodules=on-demand'
+abbr -a gsub-up 'git submodule update --remote'
 
 # git log graph
-alias gl 'git log --oneline'
-alias glg 'git log --oneline --graph --all --decorate'
+abbr -a gl 'git log --oneline'
+abbr -a glg 'git log --oneline --graph --all --decorate'
 
-alias gmi 'git_branch_name | read -l result; and git checkout $1; and git pull; and git merge $result'
+abbr -a gmi 'git_branch_name | read -l result; and git checkout $1; and git pull; and git merge $result'
 
-alias gp 'git push'
-alias gpo 'git push -u origin (git rev-parse --abbrev-ref HEAD)'
-alias gpl 'git pull'
-alias gpom 'git push -u origin master'
+abbr -a gp 'git push'
+abbr -a gpo 'git push -u origin (git rev-parse --abbrev-ref HEAD)'
+abbr -a gpl 'git pull'
+abbr -a gpom 'git push -u origin master'
 
-alias grro 'git remote rm origin '
-alias grao 'git remove add origin '
+abbr -a grro 'git remote rm origin '
+abbr -a grao 'git remove add origin '
 
 # show git commit timestamp
-alias gtm 'git show -s --format=%ci'
+abbr -a gtm 'git show -s --format=%ci'
 # show timestamp of graph search result
-alias gstm 'gl | fzf | string split " " | read -l result; and gtm $result[1]'
+abbr -a gstm 'gl | fzf | string split " " | read -l result; and gtm $result[1]'
 
 # reset to origin/undo git merge
-alias gzmh 'git reset --hard ORIG_HEAD'
+abbr -a gzmh 'git reset --hard ORIG_HEAD'
 
 # undo merge of commite using hash
-alias gzmh 'git revert -m 1'
+abbr -a gzmh 'git revert -m 1'
 
 # undo last commit
-alias gzc 'git reset --soft HEAD^'
+abbr -a gzc 'git reset --soft HEAD^'
 # redo last commit
-alias gzzc 'git commit -a -c ORIG_HEAD'
+abbr -a gzzc 'git commit -a -c ORIG_HEAD'
 
-alias gs 'git status'
+abbr -a gs 'git status'
 
-alias gss 'git stash save ' #message
-alias gsl 'git stash list'
+abbr -a gss 'git stash save ' #message
+abbr -a gsl 'git stash list'
 
-alias gco-pages 'git checkout -b gh-pages'
-alias gp-pages 'git push origin gh-pages'
-alias gpp-pages 'git checkout gh-pages ; git merge master ; git checkout master ; git push --all'
+abbr -a gco-pages 'git checkout -b gh-pages'
+abbr -a gp-pages 'git push origin gh-pages'
+abbr -a gpp-pages 'git checkout gh-pages ; git merge master ; git checkout master ; git push --all'
 
-alias gra 'git remote add'
+abbr -a gra 'git remote add'
 
 #
 # GIT EXTRAS
 #
 
-alias gf 'git feature'
-alias gff 'git feature finish'
-alias gb 'git bug'
-alias gbf 'git bug finish'
+abbr -a gf 'git feature'
+abbr -a gff 'git feature finish'
+abbr -a gb 'git bug'
+abbr -a gbf 'git bug finish'
 
 #
 # VAGRANT
 #
 
-alias vs 'vagrant status'
-alias vu 'vagrant up'
-alias vh 'vagrant halt'
-alias vr 'vagrant reload'
-alias vbu 'vagrant box update'
+abbr -a vs 'vagrant status'
+abbr -a vu 'vagrant up'
+abbr -a vh 'vagrant halt'
+abbr -a vr 'vagrant reload'
+abbr -a vbu 'vagrant box update'
 
 #
 # YARN
 #
 
-alias yai 'yarn install'
-alias yas 'yarn start'
-alias yasw 'yarn start-watch'
-alias yais 'yarn install; yarn start'
-alias yad 'yarn add'
-alias yadd 'yarn add --dev'
-alias yar 'yarn remove'
-alias yals 'yarn list --prod --depth=0'
+abbr -a yai 'yarn install'
+abbr -a yas 'yarn start'
+abbr -a yasw 'yarn start-watch'
+abbr -a yais 'yarn install; yarn start'
+abbr -a yad 'yarn add'
+abbr -a yadd 'yarn add --dev'
+abbr -a yar 'yarn remove'
+abbr -a yals 'yarn list --prod --depth=0'
 
 #
 # DOCKER
 #
 
-alias dkrb 'docker build'
-alias dkrr 'docker run'
-alias dkrps 'docker ps -l'
+abbr -a dkrb 'docker build'
+abbr -a dkrr 'docker run'
+abbr -a dkrps 'docker ps -l'
 
 # remove exited containers:
-alias dkr-prune-containers 'docker ps --filter status=dead --filter status=exited -aq | xargs -r docker rm -v'
+abbr -a dkr-prune-containers 'docker ps --filter status=dead --filter status=exited -aq | xargs -r docker rm -v'
 
 # remove unused volumes:
-alias dkr-prune-volumes 'docker volume ls -qf dangling=true | xargs -r docker volume rm'
+abbr -a dkr-prune-volumes 'docker volume ls -qf dangling=true | xargs -r docker volume rm'
 
 # remove unused images:
-alias dkr-prune-images 'docker images --no-trunc | grep "<none>" | awk "{ print $3 }" | xargs -r docker rmi'
+abbr -a dkr-prune-images 'docker images --no-trunc | grep "<none>" | awk "{ print $3 }" | xargs -r docker rmi'
 
 #
 # OTHER
 #
 
-alias tweet 'twit-cli tweet'
+abbr -a tweet 'twit-cli tweet'
 
 #
 # DIRECTORIES
 #
 
-alias cd-home 'cd $HOME'
-alias cd-projects 'cd $PROJECTS'
-alias cd-downloads 'cd $HOME/Downloads'
-alias cd-fisherman 'cd $FISHERMAN'
-alias cd-gopath 'cd $GOPATH'
-alias cd-gopath-src 'cd $GOPATH/src'
-alias cd-fish 'cd ~/.config/fish/config.fish'
+abbr -a cd-home 'cd $HOME'
+abbr -a cd-projects 'cd $PROJECTS'
+abbr -a cd-downloads 'cd $HOME/Downloads'
+abbr -a cd-fisherman 'cd $FISHERMAN'
+abbr -a cd-gopath 'cd $GOPATH'
+abbr -a cd-gopath-src 'cd $GOPATH/src'
+abbr -a cd-fish 'cd ~/.config/fish/config.fish'
