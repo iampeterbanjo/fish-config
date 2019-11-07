98a8b250e514e9c1f655d84d7e7ce39c3aebeba3
iampeterbanjo <iampeterbanjo@users.noreply.github.com>
2019-11-07 14:26:49 +0000

    fix ruby versions

--- config.fish
+++ config.fish
@@ -3,18 +3,18 @@
 #
 
 switch (uname)
-case Linux
-    echo 'Setting fish_config_path for Tux!'
-    set -U fish_config_path ~/.config/fish
-    set -U fish_config_os_aliases linux.fish
-case Darwin
-    echo 'Setting fish_config_path for Hexley!'
-    set -U fish_config_path ~/.config/fish
-    set -U fish_config_os_aliases osx.fish
-case FreeBSD NetBSD DragonFly
-    echo 'Setting fish_config_path for Beastie!'
-case '*'
-    echo 'Setting fish_config_path for, stranger!'
+    case Linux
+        echo 'Setting fish_config_path for Tux!'
+        set -U fish_config_path ~/.config/fish
+        set -U fish_config_os_aliases linux.fish
+    case Darwin
+        echo 'Setting fish_config_path for Hexley!'
+        set -U fish_config_path ~/.config/fish
+        set -U fish_config_os_aliases osx.fish
+    case FreeBSD NetBSD DragonFly
+        echo 'Setting fish_config_path for Beastie!'
+    case '*'
+        echo 'Setting fish_config_path for, stranger!'
 end
 
 set fish_color_autosuggestion yellow --bold
@@ -23,6 +23,10 @@ set -U NPM_PATH (which npm)
 
 source $fish_config_path/aliases/aliases.fish
 source $fish_config_path/aliases/$fish_config_os_aliases
+
+# fix ruby versions
+status --is-interactive
+and source (rbenv init -|psub)
 
 # enable direnv
 eval (direnv hook fish)
